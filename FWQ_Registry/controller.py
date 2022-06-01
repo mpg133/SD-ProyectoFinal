bbbe2337015455aa3ff7c9894533f1e573513fba
Carles Saez Marti <csm132@gcloud.ua.es>
2022-06-01 18:51:18 +0200

    registry separado en ficheros .py

---
+++ FWQ_Registry/controller.py
@@ -0,0 +1,62 @@
+import sqlite3
+import hashlib
+
+
+def registra(name, password):
+
+    conn = sqlite3.connect('../database.db')
+    cur = conn.cursor()
+
+    try:
+        pass_hash = hashlib.md5(bytes(password, encoding="utf8")).hexdigest()
+
+        cur.execute('insert into visitor(name, password) values("' + name + '", "'+pass_hash+'")')
+
+        conn.commit()
+
+        cur.execute('select id from visitor where name = "'+name+'"')
+        id_vis = cur.fetchall()
+        id_vis = id_vis[0][0]
+
+        conn.close()
+        return True, [str(id_vis), name]
+    except:
+        return False, ["ERROR al añadir a " + name + " al registro."]
+
+
+
+def edita(name, password, newName, newPassword):
+    conn = sqlite3.connect('../database.db')
+    cur = conn.cursor();
+
+    #el nuevo nombre ya existe
+    cur.execute('select * from visitor where name = "'+newName+'"')
+    user = cur.fetchall()
+    if len(user) >= 1:
+        conn.close()
+        return False, ["ERROR al editar visitante (nuevo nombre ocupado)."]
+
+    #el usuario a editar no existe
+    cur.execute('select * from visitor where name = "'+name+'"')
+    user = cur.fetchall()
+    if len(user) <= 0:
+        conn.close()
+        return False, ["ERROR al editar visitante (credenciales incorrectas)."]
+
+    pass_hash = hashlib.md5(bytes(password, encoding="utf8")).hexdigest()
+    new_pass_hash = hashlib.md5(bytes(newPassword, encoding="utf8")).hexdigest()
+
+    try:
+        cur.execute('update visitor set name = "'+newName+'", password = "'+new_pass_hash+'" where name = "'+name+'" and password = "'+pass_hash+'"')
+        conn.commit()
+        conn.close()
+    except:
+        return False, ["ERROR al editar usuario. (excepcion)"]
+
+    return True, [str(user[0][0]), newName, 'Visitante editado correctamente.']
+
+
+
+
+
+
--- FWQ_Registry/main.py
+++ FWQ_Registry/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from controller import *
 import hashlib
 
 import sqlite3
@@ -25,89 +26,43 @@ REGISTRY_GRPC_IP = config['REGISTRY_GRPC_IP']
 class TodoServicer(todo_pb2_grpc.TodoServicer):
     
     def registrarVisitante(self, request, context):
-            
         response = todo_pb2.RegReturns()
+        ok, resp = registra(request.name, request.password)
+        response.ok = ok
         
-        try:
-            conn = sqlite3.connect('../database.db')
-            cur = conn.cursor()
-
-            pass_hash = hashlib.md5(bytes(request.password, encoding="utf8")).hexdigest()
-            cur.execute('insert into visitor(name, password) values("' + request.name + '", "'+pass_hash+'")')
-            conn.commit()
-
-            cur.execute('select id from visitor where name = "'+request.name+'"')
-            id_vis = cur.fetchall()
-            id_vis = id_vis[0][0]
-            conn.close()
-
-            response.id = str(id_vis)
-            response.name=request.name
-            response.ok=True
-            response.msg="Usuario registrado."
-
+        if ok:
+            response.id = resp[0]
+            response.name = resp[1]
+            response.msg = "Usuario registrado."
             print('Usuario "' + request.name + '" registrado.')
-            
-        except:
-            response.ok = False
-            response.msg = "ERROR al añadir a " + request.name + " al registro."
-            
+        else:
+            response.id="-1"
+            response.name="null"
+            response.msg = resp[0]
+
         return response
+
 
     def editarVisitante(self, request, context):
-        
         response = todo_pb2.RegReturns()
+        ok, resp = edita(request.name, request.password, request.newName, request.newPassword)
+        response.ok = ok
 
-        try:
-            conn = sqlite3.connect('../database.db')
-            cur = conn.cursor();
-    
-            #el nuevo nombre ya existe
-            cur.execute('select * from visitor where name = "'+request.newName+'"')
-            user = cur.fetchall()
-
-            if len(user) >= 1:
-                response.ok = False
-                response.msg = "ERROR al editar usuario (nuevo nombre ocupado)."
-                conn.close()
-                return response
-
-            #el usuario a editar no existe
-            cur.execute('select * from visitor where name = "'+request.name+'"')
-            user = cur.fetchall()
-            if len(user) <= 0:
-                response.ok = False
-                response.msg = "ERROR al editar usuario (credenciales incorrectas)."
-                conn.close()
-                return response
-            
-
-            pass_hash = hashlib.md5(bytes(request.password, encoding="utf8")).hexdigest()
-            new_pass_hash = hashlib.md5(bytes(request.newPassword, encoding="utf8")).hexdigest()
-
-            cur.execute('update visitor set name = "'+request.newName+'", password = "'+new_pass_hash+'" where name = "'+request.name+'" and password = "'+pass_hash+'"')
-            conn.commit()
-            conn.close()
-
+        if ok:
+            response.id = resp[0]
+            response.name = resp[1]
+            response.msg = resp[2]
             print('Usuario "'+request.name+'" editado.')
-
-            response.id = str(user[0][0])
-            response.name = request.newName
-            response.ok = True
-            response.msg = "Usuario editado correctamente"
-
-            return response
-
-        except:
-            print('mal')
-            response.ok = False
-            response.msg = "ERROR al editar usuario."
+        else:
+            response.id = "-1"
+            response.name = "null"
+            response.msg=resp[0]
 
         return response
+
 
 
 def main():
-
     try:
         open('../database.db', 'rb').close()
     except:
@@ -129,10 +84,10 @@ def main():
         root_certificates=ca_cert,
         require_client_auth=True
     )
-    
   
     server.add_secure_port('[::]:' + REGISTRY_GRPC_PORT, credentials)
     server.start()
+
     print('Starting server. Listening on port ' + REGISTRY_GRPC_PORT)
 
     try:
